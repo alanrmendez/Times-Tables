a8770b32c7694b8a4611fddeeb9d4ba8de6aa165
alanrmendez <alan.mendez07@gmail.com>
2021-01-24 00:03:27 -0500

    Initial Commit

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift.
+//  Times Tables
+//
+//  Created by Alan Mendez on 1/24/21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
